b579b4dc75e64d9f02e43067088bdfe4aac22c72
VimanyuAgg <vimanyu1022007@gmail.com>
2017-05-12 14:02:05 -0700

    Updated jobSeeker model to contain user details

--- Job-Portal-Project/src/main/java/edu/cmpe275/termproject/controller/UserController.java
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/controller/UserController.java
@@ -1,10 +1,13 @@
 package edu.cmpe275.termproject.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.cmpe275.termproject.model.Company;
 import edu.cmpe275.termproject.service.CompanyService;
 import edu.cmpe275.termproject.service.UserService;
 
@@ -17,6 +20,19 @@ public class UserController {
 	@RequestMapping(value="/jobseeker/register", method=RequestMethod.GET)
 	public String getJobSeekerView(){
 		return "jobseeker-registration";
+		
+	}
+	
+	@RequestMapping(value="/jobseeker/register", method=RequestMethod.POST)
+	public String createJobSeeker(HttpServletRequest request){
+		String firstName=request.getParameter("firstName"), lastName=request.getParameter("lastName"), 
+				picture=request.getParameter("picture"),selfIntroduction=request.getParameter("selfIntroduction"), 
+				workExperience=request.getParameter("workExperience"), education=request.getParameter("education"), 
+			    skills=request.getParameter("skills"), password=request.getParameter("password"),
+			    username=request.getParameter("username"),email = request.getParameter("email");
+		JobSeeker jobSeeker=new JobSeeker(name, website, logoImageUrl, address, description, email ,password);
+		companyService.addCompany(company);
+		return "redirect:/companycreated";
 		
 	}
 }
