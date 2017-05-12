969ffd17498d9eeedf14b393867f357f9fea89b2
VimanyuAgg <vimanyu1022007@gmail.com>
2017-05-12 13:47:18 -0700

    Added jobseekerGET in userController & added userService

--- Job-Portal-Project/src/main/java/edu/cmpe275/termproject/controller/UserController.java
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/controller/UserController.java
@@ -1,5 +1,22 @@
 package edu.cmpe275.termproject.controller;
 
-public class UserController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import edu.cmpe275.termproject.service.CompanyService;
+import edu.cmpe275.termproject.service.UserService;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value="/jobseeker/register", method=RequestMethod.GET)
+	public String getJobSeekerView(){
+		return "jobseeker-registration";
+		
+	}
 }
---
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/service/UserService.java
@@ -0,0 +1,12 @@
+package edu.cmpe275.termproject.service;
+
+import org.springframework.stereotype.Service;
+
+import edu.cmpe275.termproject.dao.UserDAO;
+
+@Service
+public class UserService {
+	
+	private UserDAO userDAO; 
+	
+}
