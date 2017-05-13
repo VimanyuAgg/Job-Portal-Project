a5d73af6e193322a9b4eccaff9a2a03c0d0958ec
sidharthbhasin <sidhart.bhasin@sjsu.edu>
2017-05-12 23:13:13 -0700

    search companies query implemented

--- Job-Portal-Project/src/main/java/edu/cmpe275/termproject/dao/JobPostingDAO.java
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/dao/JobPostingDAO.java
@@ -1,9 +1,17 @@
 package edu.cmpe275.termproject.dao;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import edu.cmpe275.termproject.model.Company;
 import edu.cmpe275.termproject.model.JobPosting;
 
 public interface JobPostingDAO extends CrudRepository<JobPosting,Integer> {
+
+	@Query("SELECT j FROM JobPosting j WHERE LOWER(j.jobId) IN (:jobId)")
+    public List<JobPosting> findJobs(@Param("jobId") List<String> jobId);
 
 }
