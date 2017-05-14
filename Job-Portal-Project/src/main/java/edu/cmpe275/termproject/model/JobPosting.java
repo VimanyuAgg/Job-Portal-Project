19a7b4ae7da554097cca2c558c2c7dd4a153561c
VimanyuAgg <vimanyu1022007@gmail.com>
2017-05-13 21:40:59 -0700

    removed DatePosted attribute from jobdao

--- Job-Portal-Project/src/main/java/edu/cmpe275/termproject/model/JobPosting.java
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/model/JobPosting.java
@@ -38,17 +38,11 @@ public class JobPosting {
 		this.tempSize = 1;
 		postedOn = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 
-			datePosted = new SimpleDateFormat("yyyyMMdd_HHmmss").parse(postedOn);
+			//datePosted = new SimpleDateFormat("yyyyMMdd_HHmmss").parse(postedOn);
 		
 	}
 
-	public Date getDatePosted() {
-		return datePosted;
-	}
-
-	public void setDatePosted(Date datePosted) {
-		this.datePosted = datePosted;
-	}
+	
 
 	public Company getJobPostedByCompany() {
 		return jobPostedByCompany;
@@ -103,8 +97,6 @@ public class JobPosting {
 	@Column(name="POSTED_ON")
 	private String postedOn;
 	
-	@Column(name="DATE_POSTED")
-	private Date datePosted;
 	
 
 	@Column(name="ELIGIBILITY")
