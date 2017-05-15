b879411f99e852b1acaa5ce303d5c8a2537740e6
VimanyuAgg <vimanyu1022007@gmail.com>
2017-05-15 11:04:36 -0700

    updated jobseeker dashboard with HTTP session check

--- Job-Portal-Project/src/main/java/edu/cmpe275/termproject/controller/JobSeekerController.java
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/controller/JobSeekerController.java
@@ -19,6 +19,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -161,7 +162,7 @@ public class JobSeekerController {
 			redirectAttribute.addFlashAttribute("lastName",jobSeekerService.getJobSeeker(username).getLastName());
 			//httpSession.setAttribute("userID", userN);
 			//redirectAttribute.addFlashAttribute("username","Thank you for registering with us, "+username);
-			return "redirect:/jobseeker/dashboard";
+			return "redirect:/jobseeker/"+username+"/dashboard";
 		}
 		else
 		return "redirect:/jobseeker/login"; 
@@ -171,14 +172,17 @@ public class JobSeekerController {
 	
 
 	//DASHBOARD - GET
-	@RequestMapping(value="/jobseeker/dashboard",method=RequestMethod.GET)
+	@RequestMapping(value="/jobseeker/{username}/dashboard",method=RequestMethod.GET)
 	public String jobSeekerDashBoard(@ModelAttribute("selfIntroduction") String selfIntroduction,
 									 @ModelAttribute("firstName") String firstName,
 									 @ModelAttribute("lastName") String lastName,
 									 @ModelAttribute("picture") String picture,
-									 @ModelAttribute("topJobs") ArrayList<JobPosting> topJobs
-									 ){
+									 @ModelAttribute("topJobs") ArrayList<JobPosting> topJobs,
+									 @PathVariable String username ){
 		System.out.println("Inside GET Jobseeker");
+		if(httpSession.getAttribute(username) !=username){
+			return "redirect:/jobseeker/login";
+		}
 		return "jobseeker-dashboard";
 	}
 	
