48149e1297f3071f2ba0fd424657d9cd201d0900
Vimanyu Aggarwal <vimanyu1022007@gmail.com>
2017-05-25 22:55:50 -0700

    removing credentials

--- Job-Portal-Project/src/main/java/edu/cmpe275/termproject/emailService/RegistrationEmail.java
+++ Job-Portal-Project/src/main/java/edu/cmpe275/termproject/emailService/RegistrationEmail.java
@@ -27,7 +27,7 @@ public class RegistrationEmail {
 //		String emailPassword = "Testouth1@";		
 //		
 		String emailID = "springonionscmpe275@gmail.com";
-		String emailPassword = "spring-onions123";
+		String emailPassword = ###CONTACT COLLABORATORS ###;
 		
 		Properties properties = new Properties();
 		properties.put("mail.smtp.auth", "true");
